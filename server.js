de8a64d8bb36af1f0b4f259e1e68d0c3efc180d4
Alan Triacca <atriacca@yahoo.com>
2019-05-01 10:25:04 -0600

    commit to push server.js boilerplate

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 7000
 
-app.listen(7000, () => {
-    console.log("Server is running on Port 7000")
+// Middlewares for every request 
+app.use(express.json()) // req.body = Object from POST and PUT requests
+app.use(morgan('dev'))
+
+// Connect to mongoDB
+mongoose.connect("mongodb://localhost:27017/expenses", {useNewUrlParser: true}, () => {
+    console.log("connected to the DB")
+})
+
+// Routes - Endpoints
+app.use("/expenses", require('./routes/expenseRouter.js'))
+
+// Global Server Error Handler - handles ANY thrown error from ANY of our routes above
+app.use((err, req, res, next) => {
+    console.error(err)
+    return res.send({errMsg: err.message})
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
